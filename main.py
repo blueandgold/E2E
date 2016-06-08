684996daf0b0b00199563e4f21588a8d6f7586a6
Henry Chang <henryc@henryc-macbookair.roam.corp.google.com>
2016-06-08 16:37:17 -0700

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello, webapp2!')
+        self.response.write('Hello, webapp23!\n')
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
@@ -10,7 +10,7 @@ app = webapp2.WSGIApplication([
 
 def main():
     from paste import httpserver
-    httpserver.serve(app, host='127.0.0.1', port='8080')
+    httpserver.serve(app, host='0.0.0.0', port='8080')
 
 if __name__ == '__main__':
     main()
