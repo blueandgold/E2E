7c7db388702db077fc24ac33d28d01be22ad3d99
Henry Chang <henryc@henryc-macbookair2.roam.corp.google.com>
2016-08-03 14:24:24 -0700

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello, webapp88888!\n\n')
+        self.response.write('Hello, webapp6666!\n\n')
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
