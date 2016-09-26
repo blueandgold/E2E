110cd80ed7fb1a1ec42663824035ea2856ae4e6a
Henry Chang <henryc@henryc-macbookair2.roam.corp.google.com>
2016-09-26 16:52:23 -0700

    testing

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import sys
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('[%(levelname)s] %(message)s')
+stdout_handler.setFormatter(formatter)
+logger.addHandler(stdout_handler)
 
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
  
-        logger.info('>>>>> aaaaa')
+        logger.info('>>>>> bbbbb')
 
         project_id = 'henry-dev'
         service_account_id = 'test123@henry-dev.iam.gserviceaccount.com'
